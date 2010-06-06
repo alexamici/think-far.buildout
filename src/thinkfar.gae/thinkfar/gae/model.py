b01acdf73e604766519a71fb1d09c02ad91529da
Alessandro Amici <a.amici@bopen.it>
2010-06-07 01:21:15 +0200

    new: tests are working (and cleanup)

--- src/thinkfar.gae/thinkfar/gae/model.py
+++ src/thinkfar.gae/thinkfar/gae/model.py
@@ -4,7 +4,7 @@ from google.appengine.ext import db
 
 class Account(db.Model):
     name = db.StringProperty(required=True)
-    parent = SelfReferenceProperty()
+    group_under = db.SelfReferenceProperty()
     order_number = db.FloatProperty()
 
 class Asset(db.Model):
@@ -15,23 +15,23 @@ class Asset(db.Model):
     sell_price = db.FloatProperty()
     sell_date = db.DateProperty()
 
-class AssetValues(db.model):
+class AssetValues(db.Model):
     estimated_market_ask = db.FloatProperty()
     estimated_market_bid = db.FloatProperty()
 
 class Liability(db.Model):
     name = db.StringProperty(required=True)
-    parent = SelfReferenceProperty()
+    group_under = db.SelfReferenceProperty()
     outstanding_debt = db.FloatProperty()
     market_price = db.FloatProperty()
     owner = db.UserProperty()
 
+def tryout():
+    assets = db.GqlQuery("SELECT * FROM Asset WHERE owner = :owner", 'ale')
+    liabilities = db.GqlQuery("SELECT * FROM Liability WHERE owner = :owner", 'ale')
 
-assets = db.GqlQuery("SELECT * FROM Asset WHERE owner = :owner", 'ale')
-liabilities = db.GqlQuery("SELECT * FROM Liability WHERE owner = :owner", 'ale')
+    assets_market_total = sum(a.market_price for a in assets)
+    assets_repurcase_total = sum(a.repurcase_price for a in assets)
+    liabilities_outstanding_total = sum(l.outstanding_debt for l in liabilities)
 
-assets_market_total = sum(a.market_price for a in assets)
-assets_repurcase_total = sum(a.repurcase_price for a in assets)
-liabilities_outstanding_total = sum(l.outstanding_debt for l in liabilities)
-
-print assets_market_total - liabilities_outstanding_total
+    print assets_market_total - liabilities_outstanding_total
