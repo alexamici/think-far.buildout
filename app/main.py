5bfdf0a20f0150761d347bda9fe31e98f16987e6
Alessandro Amici <alexamici@gmail.com>
2011-09-18 02:05:34 +0200

    bugfix: fix for a pkg_resources related problemx

--- app/main.py
+++ app/main.py
@@ -10,6 +10,17 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 
 from thinkfar.run import app
 
+# No idea what it does, but solves a pkg_resource NullProvider issue
+# taken stright from a comment to: http://code.google.com/p/bfg-pages/wiki/PyramidTutorial
+try:
+    import pkg_resources
+except ImportError:
+    pass
+else:
+    if hasattr(os, '__loader__'):
+        # This only seems to apply to the SDK
+        pkg_resources.register_loader_type(type(os.__loader__), pkg_resources.DefaultProvider)
+
 settings = {
     'reload_templates': 'true',
     'debug_authorization': 'true',
