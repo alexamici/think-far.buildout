a60dad9def39bae96272e5c424d58aa2fc69cac4
Alessandro Amici <a.amici@bopen.it>
2011-10-18 20:07:17 +0200

    new: start the migration from pyramid to webapp2 (safer on GAE)

--- app/main.py
+++ app/main.py
@@ -6,34 +6,11 @@ import sys,os
 
 sys.path.insert(0,'lib/dist')
 
-from google.appengine.ext.webapp.util import run_wsgi_app
-
 from thinkfar.run import app
 
-# No idea what it does, but solves a pkg_resource NullProvider issue
-# taken stright from a comment to: http://code.google.com/p/bfg-pages/wiki/PyramidTutorial
-try:
-    import pkg_resources
-except ImportError:
-    pass
-else:
-    if hasattr(os, '__loader__'):
-        # This only seems to apply to the SDK
-        pkg_resources.register_loader_type(type(os.__loader__), pkg_resources.DefaultProvider)
-
-settings = {
-    'reload_templates': 'true',
-    'debug_authorization': 'true',
-    'debug_notfound': 'true',
-    'debug_templates': 'true',
-    'default_locale_name': 'en',
-}
 
 def main():
-    """ This function runs a Pyramid WSGI application.
-    """
-    run_wsgi_app(app())
-
+    app.run()
 
 if __name__ == '__main__':
-    main() 
+    main()
